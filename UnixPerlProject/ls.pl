38137028d97cf7d5349eb8bd850ed2482856be1e
dvirs <dvir.sofer@walla.com>
2014-10-20 17:10:11 +0200

    fix ls -l and add ls -al

--- UnixPerlProject/ls.pl
+++ UnixPerlProject/ls.pl
@@ -3,6 +3,7 @@ use Switch;
 use JSON;
 use File::stat;
 use Time::localtime;
+use Fcntl ':mode';
 
 $command = $ARGV[0];    #command
 $dirname = $ARGV[1];    #path for dir
@@ -19,7 +20,7 @@ switch ($command) {
 		@sorted_files = sort @files;
 		print encode_json( \@sorted_files );
 	}
-	case "-l" {
+	case "-l" { # fix print all files
 
 		#ls -l
 		my @files = readdir $dh;
@@ -27,48 +28,37 @@ switch ($command) {
 
 		@sorted_files = sort @files;
 
-		#print encode_json( \@sorted_files );
-		my $str         = "";
-		my $permissions = "-";
 		my $count = 0;
+		my $str   = "";
+		my $info = "";
+		@arrOfStrFiles = ();
 		foreach $item (@sorted_files) {
-			if ( -x $item && !-d $item )
-			{    # if file is executable and not directory
-				$sb = stat($item);
-				if ( -r $item ) {
-					$permissions = $permissions . "r";
-				}
-				else {
-					$permissions = $permissions . "-";
-				}
-				if ( -w $item ) {
-					$permissions = $permissions . "w";
-				}
-				else {
-					$permissions = $permissions . "-";
-				}
-				$permissions = $permissions . "x------";    #fix it
-
-				my $nlink = $sb->nlink;
-
-				my $uid  = ( stat $item )[4];               #number of user
-				my $user = ( getpwuid $uid )[0];            #name of user
-
-				my $gid   = ( stat $item )[4];              #number of group
-				my $group = ( getpwuid $gid )[0];           #name of group
-				
-				my $size = -s $item;
-
-				my $date = ctime( $sb->atime );
-
-				$str =
-				  $str . "$permissions $nlink $user $group $size $date $item \n";
-			}
+			$str .= getFileInformation($item);
 			$count++;
 		}
-		print "total $count\n";
-		print $str;
+		#unshift(@arrOfStrFiles, total $count);
+		$str = "total $count\n" . $str;
+		@arrOfStrFiles = split('\n', $str);
+		print encode_json( \@arrOfStrFiles );
+		#print "total $count\n$str";
 	}
+	case "-al" {
+
+		#ls -al
+		@files = readdir $dh;
+		closedir $dh;
+
+		@sorted_files = sort @files;
+
+		my $count = 0;
+		my $str   = "";
+		foreach $item (@sorted_files) {
+			$str .= getFileInformation($item);
+			$count++;
+		}
+		print "total $count\n$str";
+	}
+
 	else {
 
 		#ls
@@ -78,7 +68,68 @@ switch ($command) {
 		@sorted_files = sort @files;
 		print encode_json( \@sorted_files );
 	}
+	print "\n"
 }
 
-print "\n";
+sub getPermissions {
+	$permissions = "";
+	foreach $item (@_) {
+		if ( $item == 0 ) {
+			$permissions = $permissions . "---";
+		}
+		elsif ( $item == 1 ) {
+			$permissions = $permissions . "--x";
+		}
+		elsif ( $item == 2 ) {
+			$permissions = $permissions . "-w-";
+		}
+		elsif ( $item == 3 ) {
+			$permissions = $permissions . "-wx";
+		}
+		elsif ( $item == 4 ) {
+			$permissions = $permissions . "r--";
+		}
+		elsif ( $item == 5 ) {
+			$permissions = $permissions . "r-x";
+		}
+		elsif ( $item == 6 ) {
+			$permissions = $permissions . "rw-";
+		}
+		elsif ( $item == 7 ) {
+			$permissions = $permissions . "rwx";
+		}
+	}
+
+	return $permissions;
+}
+
+sub getFileInformation {
+	my $str         = "";
+	my $permissions = "-";
+	foreach $item (@_) {
+		$sb = stat($item);
+
+		my $mode = $sb->mode & 07777;
+		my $usr  = ( $mode & 0700 ) >> 6;    #mode of user
+		my $grp  = ( $mode & 0070 ) >> 3;    #mode of group
+		my $oth  = $mode & 0007;             #mode of others
+
+		$permissions .= getPermissions( $usr, $grp, $oth );
+
+		my $nlink = $sb->nlink;
+
+		my $uid  = ( stat $item )[4];        #number of user
+		my $user = ( getpwuid $uid )[0];     #name of user
+
+		my $gid   = ( stat $item )[4];       #number of group
+		my $group = ( getpwuid $gid )[0];    #name of group
+
+		my $size = -s $item;                 #size of file
+
+		my $date = ctime( $sb->atime );
+
+		$str .= "$permissions $nlink $user $group $size $date $item \n";
+	}
+	return $str;
+}
 
