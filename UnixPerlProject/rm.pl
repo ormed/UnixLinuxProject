62303f343d2da9549f1858b964247c77fde8b120
avmdvs <avishay89@walla.com>
2014-11-08 19:39:06 +0200

    add remove file recursive

--- UnixPerlProject/rm.pl
+++ UnixPerlProject/rm.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 use Switch;
+use JSON;
 
 use strict;
 use warnings;
@@ -7,15 +8,37 @@ use warnings;
 my $command = $ARGV[0];    #command
 my $dirname = $ARGV[1];    #path for dir
 
-my @error_arr = ("ls: cannot access $dirname: No such file or directory");
-opendir my ($dh), $dirname or die print encode_json( \@error_arr );
+my @error_arr = ("cannot access $dirname: No such file or directory");
 
-my @all_files      = readdir $dh;
+open my ($df), $dirname or die print encode_json( \@error_arr );
+
+my $dh;
+
+if ( -f $df ) {
+	unlink $dirname or warn "Could not unlink $dirname: $!";
+	exit;
+} 
+
+opendir $dh, $dirname or die print encode_json(\@error_arr);
+my @all_files = readdir $dh;
+my @un_dotted_files = grep { !/^\.\./ } @all_files;
 closedir $dh;
 
-switch ($command) {
-	case "-a" {
 
+switch ($command) {
+	case "-r" {
+		if ($#un_dotted_files > 0) {
+			foreach my $item (@un_dotted_files) {
+				if ( $item eq '.') {
+					next;
+				} elsif ( $item eq '..') {
+					next;
+				} else {
+					removeFolderContent($dirname . $item);	
+				}
+			}	
+		}
+		rmdir $dirname;
 	}
 
 	else {
@@ -26,4 +49,55 @@ switch ($command) {
 		}
 	}
 }
+
+
+sub removeFolderContent {
+	my @params = @_;
+	my $current_directory = $params[0];
+	my @temp_files;
+	
+	print $current_directory . "\n";
+	
+	#stop condition if we reach our 'root' directory
+	if ( $current_directory eq $dirname ) {
+		return;
+	}
+	
+	#stop condition if its a file
+	if ( -f $current_directory ) {
+		unlink $current_directory;
+		$current_directory =~ s/\/([\x00-\x2E\x30-\x7F]+)$/\//g;	# remove file name from path
+		removeFolderContent($current_directory);
+		return;
+	}
+	
+	if ( -d $current_directory ) {
+		opendir $dh, $current_directory or die print (\@error_arr);
+		@temp_files = readdir $dh;
+		close $dh;
+		
+		# safety - stopping condition if we reached an empty folder.
+		my $size = $#temp_files;
+		if ($size == 0 ) {
+			rmdir $current_directory;
+			return;
+		}
+		
+		foreach my $item (@temp_files) {
+			if ( $item eq '.') {
+				next;
+			} elsif ( $item eq '..') {
+				next;
+			} else {
+				removeFolderContent($current_directory . '/' . $item);	
+			}
+		}
+		
+		rmdir $current_directory;
+		return;
+	}
+	
+}
+
+
 
