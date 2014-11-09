0c4fc4081370c8b18568904f9f00e9bc0f849678
avmdvs <avishay89@walla.com>
2014-11-10 01:18:00 +0200

    mroe changes to file browser

--- UnixPhpProject/file_browser.php
+++ UnixPhpProject/file_browser.php
@@ -1,41 +1,48 @@
-<?php include_once ('parts/top.php');?>
+<?php
+include_once ('parts/top.php');
+?>
 
 <div id="wrapper">
 
-	<?php include_once ('parts/nav.php');?>
+	<?php
+	include_once ('parts/nav.php');
+	?>
 
 	<div id="page-wrapper">
 		<div class="container-fluid">
-			
+
 			<ul class='custom-menu'>
-  				<li data-action="first">First thing</li>
-  				<li data-action="second">Second thing</li>
-  				<li data-action="third">Third thing</li>
+				<li data-action="first">First thing</li>
+				<li data-action="second">Second thing</li>
+				<li data-action="third">Third thing</li>
 			</ul>
-			
-			<div class="folder">
-				<input type="image" src="images/fileicons/_Documents.png" class="btn-folder" ondblclick="alert('double click')">
-				<label class="text-center">Folder Name</label>
+
+			<input type="hidden" id="current-folder" name="current-folder" value="/home/">
+			<input type="hidden" id="prev-folder" name="prev-folder" value="/">
+			<input type="hidden" id="folder-option" name="folder-option" value="-al">
+
+			<div id="file-browser" class="col-md-6">
+				
+					<div class="file col-md-1">
+						<input type="image" src="images/fileicons/default.png" class="btn-file">
+						<label class="text-center">Short text</label>
+					</div>
 			</div>
-			
-			<div class="file">
-				<input type="image" src="images/fileicons/default.png" class="btn-file" ondblclick="alert('double click')">
-				<label class="text-center">a very fucking long file name</label>
-			</div>
-			
-			<div class="file">
-				<input type="image" src="images/fileicons/default.png" class="btn-file" ondblclick="alert('double click')">
-				<label class="text-center">Short text</label>
-			</div>
-						
+
 		</div>
 		<!-- /#container-fuiid -->
-		
+
 	</div>
 	<!-- /#page-wrapper -->
-	
+
 </div>
 <!-- /#wrapper -->
 <script src="js/file_manager.js"></script>
+<script>
+	var path = $('#current-folder').val();
+	showFolders(path);
+</script>
 
-<?php include_once('parts/bottom.php'); ?>
+<?php
+	include_once ('parts/bottom.php');
+?>
--- UnixPhpProject/js/file_manager.js
+++ UnixPhpProject/js/file_manager.js
@@ -1,6 +1,5 @@
 // Trigger action when the contexmenu is about to be shown
-$('.btn-folder').bind("contextmenu", function (event) {
-    
+$(document).on("contextmenu", ".btn-folder", function (event) {
     // Avoid the real one
     event.preventDefault();
     
@@ -28,7 +27,7 @@ $(document).bind("mousedown", function (e) {
 
 
 // If the menu element is clicked
-$(".custom-menu li").click(function(){
+$('.custom-menu li').click(function(){
     
     // This is the triggered action name
     switch($(this).attr("data-action")) {
@@ -42,3 +41,87 @@ $(".custom-menu li").click(function(){
     // Hide it AFTER the action was triggered
     $(".custom-menu").hide(100);
   });
+
+// Perform enter folder event
+function showFolders(path) {
+	var $option = $('#folder-option').val();
+	var url = 'processes/perl_commands_exec.php';
+	var data = { page : 'ls', option : $option, path : path };
+	$.ajax({
+		type : 'POST',
+		url : url,//proccess - server
+		data : data,
+		dataType : 'json',
+		success : function(data) {
+			
+			//update prev folder
+			var temp_path = path.substring(0, path.length - 1);
+			temp_path = temp_path.substring(0, temp_path.lastIndexOf("/")) + '/';
+			$('#prev-folder').val(temp_path);
+			
+			data.shift(); //remove first from result
+			$('#file-browser').empty();
+			
+			$.each(data, function(key, value) {
+				var line = value.split(",");
+				var file_name = line[line.length-1];
+				var file_type = line[0].charAt(0); 
+				var new_file = '';
+				switch (file_type) {
+			    case '-':
+			    	new_file = getFileString(path, file_name);
+			        break;
+			    case 'l':
+			    	new_file = getFolderString(path, file_name);
+			        break;
+			    case 'd':
+			    	new_file = getFolderString(path, file_name);
+			        break;
+			    case 'p':
+			        break;
+			    case 's':
+			        break;
+			    case 'c':
+			        break;
+			    case 'b':
+			        break;
+			    case 'D':
+			        break;
+				}
+				
+				if (new_file != '') {
+					$('#file-browser').append(new_file);
+				}
+				
+			});
+		}
+	});
+}
+
+function getFolderString(path, folder_name) {
+	var folder = '<div class="folder col-md-1">';
+	if (folder_name == '.') {
+		return '';
+	} else if (folder_name == '..') {
+		var prev_path = $('#prev-folder').val();
+		folder += '<input type="image" src="images/fileicons/_Documents.png" ondblclick="showFolders(\'' + prev_path + '\');" class="btn-folder" data-path="' + prev_path + ' ">';
+	} else {
+		folder += '<input type="image" src="images/fileicons/_Documents.png" ondblclick="showFolders(\'' + path + folder_name + '/\');" class="btn-folder" data-path="' + path + folder_name + '/">';
+	}
+	folder += '<label class="text-center">' + folder_name + '</label>';
+	folder += '</div>';
+
+	return folder;
+}
+
+
+function getFileString(path, file_name) {
+	var folder = '<div class="folder col-md-1">';
+	folder += '<input type="image" src="images/fileicons/default.png" ondblclick="" class="btn-folder" data-path="' + path + file_name + '/">';
+	folder += '<label class="text-center">' + file_name + '</label>';
+	folder += '</div>';
+
+	return folder;
+}
+
+
