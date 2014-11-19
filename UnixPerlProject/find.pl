e1dcbf556e3e3dca7249363e8e397a3d8e4fba1b
dvirs <dvir.sofer@walla.com>
2014-11-19 19:18:42 +0200

    add find command

--- UnixPerlProject/find.pl
+++ UnixPerlProject/find.pl
@@ -5,40 +5,57 @@ use JSON;
 use strict;
 use warnings;
 
-my $dirname = $ARGV[0];    #command
-my $find    = $ARGV[1];    #name of dir/file we want find
+#my $dirname = $ARGV[0];    #command
+my $find = $ARGV[0];    #name of dir/file we want find
 
-#my $dirname   = "~";
+my $dirname   = "/";
 my @error_arr = ("error");
+my $dh;
 
-open my $dh, $dirname or die print encode_json( \@error_arr );
-
+opendir $dh, $dirname or die print encode_json( \@error_arr );
+my @all_files = readdir $dh;
 closedir $dh;
 
-findFileOrFolder( $dh, $find );
+foreach my $item (@all_files) {
+	if ( $item eq '.' ) {
+		next;
+	}
+	elsif ( $item eq '..' ) {
+		next;
+	}
+	else {
 
-sub fineFileOrFolder {
+		$item = "/" . $item . "/";
+		findFileOrFolder( $item, $find );
+	}
+}
+
+sub findFileOrFolder {
 	my @params            = @_;
 	my $current_directory = $params[0];    #current directory
 	my $target            = $params[1];    #name of file we want find
 
 	my @temp_files;
 
+	my $dh;
+
+	#print("$current_directory\n");
 	#stop condition if its a file
 	if ( -f $current_directory ) {
 		if ( $target eq $current_directory ) {
-			print($current_directory);
+			print("$current_directory\n");
 			return;
 		}
 	}
 
 	if ( -d $current_directory ) {
 		if ( $target eq $current_directory ) {
-			printf($current_directory);
+			print("$current_directory\n");
 		}
 		opendir $dh, $current_directory or die print( \@error_arr );
 		@temp_files = readdir $dh;
 		close $dh;
+		
 
 		# safety - stopping condition if we reached an empty folder.
 		my $size = $#temp_files;
@@ -47,11 +64,22 @@ sub fineFileOrFolder {
 		}
 
 		foreach my $item (@temp_files) {
-			if ( $item eq '..' ) {
+			if ( $item eq '.' ) {
+				next;
+			}
+			elsif ( $item eq '..' ) {
 				next;
 			}
 			else {
-				fineFileOrFolder( $current_directory, $target );
+				if ( $item eq $target ) {
+					if(-d $item) {
+						print($current_directory . $item . "\n");
+					}
+					else {
+						print($current_directory . "/" . $item . "\n");
+					}		
+				}
+				findFileOrFolder( $current_directory . $item, $target );
 			}
 		}
 	}
