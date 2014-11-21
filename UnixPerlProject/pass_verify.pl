1bfd1bd50e6fb6ff744781e4a36027db7e48d8a8
avmdvs <avishay89@walla.com>
2014-11-21 22:33:28 +0200

    finish user login page

--- UnixPerlProject/pass_verify.pl
+++ UnixPerlProject/pass_verify.pl
@@ -1,16 +1,32 @@
 #!/usr/bin/perl
-use JSON;
-
 use strict;
 use warnings;
 
-my $password = $ARGV[0];    #password to verify
+use constant false => 0;
+use constant true  => 1;
+
+my $user_name = $ARGV[0];   #user name to varify password
+my $password = $ARGV[1];    #password to verify
 
 #find hash from shadow file
+my $dirname  = '/etc/shadow';
 my @error_arr = ("$dirname: No such file or directory");
 open my ($dh), '<:utf8', $dirname or die print encode_json( \@error_arr );
 
 my $text_file = do { local $/; <$dh> };
 
+my @full_hash = $text_file =~ qr/$user_name:(.+?):/;
 
-my $crypt_pass = crypt($password,"\$6\$fWNQdUdz\$");
+if (@full_hash) {
+	my @salt = $full_hash[0] =~ qr/^\$6\$([^\$].*)\$/;
+
+	my $crypt_pass = crypt($password,"\$6\$$salt[0]\$");
+
+	if ($full_hash[0] eq $crypt_pass) {
+		print true;
+		exit;
+	}
+}
+
+print false;
+
--- UnixPhpProject/login.php
+++ UnixPhpProject/login.php
@@ -7,14 +7,27 @@ if (isset($_SESSION['user'])) {
 
 require_once ("parts/top.php");
 
+$error = '';
+
 if (($_SERVER["REQUEST_METHOD"] == "POST")) {
-//insert user to session
-$user = cleanInput($_POST['username']);
-$_SESSION['user'] = $user;
+	//insert user to session
+	$user = cleanInput($_POST['username']);
+	$pass = cleanInput($_POST['password']);
 
-header('Location: index.php');
+	if (empty($user) || empty($pass)) {
+		$error = 'Please fill all the form';
+	} else {
+	
+		$result = shell_exec('sudo perl /var/www/html/UnixLinuxProject/UnixPerlProject/pass_verify.pl ' . $user . ' ' . $pass);
 
-} else {
+		if ($result) {
+			$_SESSION['user'] = $user;
+			header('Location: index.php');
+		} else {
+			$error = 'The user or password you entered is incorrect.';
+		}
+	}
+} 
 ?>
 
 <div id="wrapper">
@@ -22,19 +35,28 @@ header('Location: index.php');
 	<div id="page-wrapper">
 
 		<div class="container-fluid">
-		
-			<form class="form-horizontal col-lg-3" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> role="form" method="post">
-				<div class="form-group">
-					<label for="inputUser">User Name</label>
-					<input type="text" class="form-control" name="username" id="username" placeholder="Enter User name">
-				</div>
-				<div class="form-group">
-					<label for="InputPass">Password</label>
-					<input type="password" class="form-control" name="password" id="password" placeholder="Password">
-				</div>
-				<button type="submit" class="btn btn-primary">Submit </button>
-			</form>
+			
+			<div class="col-lg-10">
+				<div class="alert alert-danger col-lg-4">
+        			<a href="#" class="close" data-dismiss="alert">&times;</a>
+        			<strong>Error!</strong> <?php echo($error); ?>
+    			</div>
+			</div>
 
+			<div class="col-lg-10">
+				<form class="form-horizontal col-lg-4" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> role="form" method="post">
+					<div class="form-group">
+						<label for="inputUser">User Name</label>
+						<input type="text" class="form-control" name="username" id="username" placeholder="Enter User name" value="<?php echo(isset($user) ? $user : ''); ?>">
+					</div>
+					<div class="form-group">
+						<label for="InputPass">Password</label>
+						<input type="password" class="form-control" name="password" id="password" placeholder="Password">
+					</div>
+					<button type="submit" class="btn btn-primary">Submit </button>
+				</form>
+			</div>
+		
 		</div>
 		<!-- /.container-fluid -->
 
@@ -44,7 +66,4 @@ header('Location: index.php');
 </div>
 <!-- /#wrapper -->
 
-<?php
-}
-require_once ("parts/bottom.php");
-?>
+<?php require_once ("parts/bottom.php"); ?>
